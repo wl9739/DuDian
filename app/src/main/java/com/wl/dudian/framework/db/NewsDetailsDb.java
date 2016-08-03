15ad9623d16b892fbd21f0fe38157a44e8ce4036
Qiushui <wl9739@gmail.com>
2016-08-03 20:04:09 +0800

    update for prepare refactor

--- app/src/main/java/com/wl/dudian/framework/db/NewsDetailsDb.java
+++ app/src/main/java/com/wl/dudian/framework/db/NewsDetailsDb.java
@@ -13,4 +13,52 @@ public class NewsDetailsDb extends RealmObject {
     private String image;
     private String share_url;
     private String ga_prefix;
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getImage_source() {
+        return image_source;
+    }
+
+    public void setImage_source(String image_source) {
+        this.image_source = image_source;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getShare_url() {
+        return share_url;
+    }
+
+    public void setShare_url(String share_url) {
+        this.share_url = share_url;
+    }
+
+    public String getGa_prefix() {
+        return ga_prefix;
+    }
+
+    public void setGa_prefix(String ga_prefix) {
+        this.ga_prefix = ga_prefix;
+    }
 }
