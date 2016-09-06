b0ecd35a64e081ce887270695608c8c0d8611bfc
Qiushui <wl9739@gmail.com>
2016-09-06 15:42:30 +0800

    删除分享

--- app/src/main/java/com/wl/dudian/app/newsdetail/NewsDetailPresenter.java
+++ app/src/main/java/com/wl/dudian/app/newsdetail/NewsDetailPresenter.java
@@ -11,7 +11,6 @@ import com.wl.dudian.framework.Variable;
 import java.util.concurrent.TimeUnit;
 
 import cn.sharesdk.framework.ShareSDK;
-import cn.sharesdk.onekeyshare.OnekeyShare;
 import rx.Observable;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
@@ -93,6 +92,11 @@ public class NewsDetailPresenter implements NewsDetailContract.Presenter {
         domainService.saveToFavoriteDb(newsDetails);
     }
 
+    @Override
+    public void share() {
+
+    }
+
     /**
      * 处理特殊的新闻页面
      *
@@ -167,35 +171,6 @@ public class NewsDetailPresenter implements NewsDetailContract.Presenter {
                     }
                 });
     }
-
-    @Override
-    public void share() {
-        ShareSDK.initSDK(context);
-        OnekeyShare oks = new OnekeyShare();
-        //关闭sso授权
-        oks.disableSSOWhenAuthorize();
-        // 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
-        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
-        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
-        oks.setTitle("");
-        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
-        oks.setTitleUrl("");
-        // text是分享文本，所有平台都需要这个字段
-        oks.setText("读点日报 -- 闲暇时间, 读点日报");
-        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
-        // oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
-        // url仅在微信（包括好友和朋友圈）中使用
-        oks.setUrl("");
-        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
-        // oks.setComment("我是测试评论文本");
-        // site是分享此内容的网站名称，仅在QQ空间使用
-        // oks.setSite(getString(R.string.app_name));
-        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
-        // oks.setSiteUrl("http://sharesdk.cn");
-        // 启动分享GUI
-        oks.show(context);
-    }
-
     @Override
     public void updateRead(final int id) {
 //        domainService.updateRead(id);
